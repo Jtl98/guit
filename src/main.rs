bc42da95198a25e8ff39f3b9a9673a55e447b4b6
josh <joshualewisvic@gmail.com>
2026-02-22 15:45:46 +1100

    create app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+
+use crate::app::App;
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        ..Default::default()
+    };
+
+    eframe::run_native("guit", options, Box::new(|_cc| Ok(Box::<App>::default())))
 }
