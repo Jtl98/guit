0fd7e5dc17e1522c2cc4433f07322f4284c4516e
Test User <test@example.com>
2026-03-13 10:10:37 +1100

    add recent repo unit tests

--- src/config.rs
+++ src/config.rs
@@ -68,7 +68,7 @@ impl Config {
     }
 }
 
-#[derive(Deserialize, Eq, Serialize)]
+#[derive(Deserialize, Debug, Eq, Serialize)]
 pub struct RecentRepo {
     pub path: PathBuf,
     opened: u64,
@@ -94,5 +94,87 @@ impl Hash for RecentRepo {
 impl PartialEq for RecentRepo {
     fn eq(&self, other: &Self) -> bool {
         self.path == other.path
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::hash_map::DefaultHasher;
+
+    #[test]
+    fn recent_repo_new() {
+        let path = PathBuf::from("/test/repo");
+        let repo = RecentRepo::new(path.clone());
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        assert_eq!(repo.path, path);
+        assert!(repo.opened <= now);
+        assert!(repo.opened >= now.saturating_sub(1));
+    }
+
+    #[test]
+    fn recent_repo_equality_same_path() {
+        let path = PathBuf::from("/test/repo");
+        let repo1 = RecentRepo::new(path.clone());
+        let repo2 = RecentRepo::new(path);
+
+        assert_eq!(repo1, repo2);
+    }
+
+    #[test]
+    fn recent_repo_inequality_different_paths() {
+        let repo1 = RecentRepo::new(PathBuf::from("/test/repo1"));
+        let repo2 = RecentRepo::new(PathBuf::from("/test/repo2"));
+
+        assert_ne!(repo1, repo2);
+    }
+
+    #[test]
+    fn recent_repo_hash_equality_same_path() {
+        let path = PathBuf::from("/test/repo");
+        let repo1 = RecentRepo::new(path.clone());
+        let repo2 = RecentRepo::new(path);
+        let mut hasher1 = DefaultHasher::new();
+        let mut hasher2 = DefaultHasher::new();
+
+        repo1.hash(&mut hasher1);
+        repo2.hash(&mut hasher2);
+
+        let hash1 = hasher1.finish();
+        let hash2 = hasher2.finish();
+
+        assert_eq!(hash1, hash2);
+    }
+
+    #[test]
+    fn recent_repo_hash_inequality_different_paths() {
+        let repo1 = RecentRepo::new(PathBuf::from("/test/repo1"));
+        let repo2 = RecentRepo::new(PathBuf::from("/test/repo2"));
+        let mut hasher1 = DefaultHasher::new();
+        let mut hasher2 = DefaultHasher::new();
+
+        repo1.hash(&mut hasher1);
+        repo2.hash(&mut hasher2);
+
+        let hash1 = hasher1.finish();
+        let hash2 = hasher2.finish();
+
+        assert_ne!(hash1, hash2);
+    }
+
+    #[test]
+    fn recent_repo_equality_ignores_opened() {
+        let path = PathBuf::from("/test/repo");
+        let mut repo1 = RecentRepo::new(path.clone());
+        let mut repo2 = RecentRepo::new(path);
+
+        repo1.opened = 1000;
+        repo2.opened = 2000;
+
+        assert_eq!(repo1, repo2);
     }
 }
