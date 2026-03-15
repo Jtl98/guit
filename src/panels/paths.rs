8a59351453f08c32db72e45fd996a3318a4d1045
josh <joshualewisvic@gmail.com>
2026-03-15 20:10:41 +1100

    use collapsing headers for unstaged/staged areas

--- src/panels/paths.rs
+++ src/panels/paths.rs
@@ -2,7 +2,7 @@ use crate::{
     common::{Action, DiffKey, Diffs, RepoAction::AddOrRestore},
     panels::Show,
 };
-use eframe::egui::{Context, Key, ScrollArea, SidePanel, Ui};
+use eframe::egui::{CollapsingHeader, Context, Key, ScrollArea, SidePanel, Ui};
 
 pub struct PathsPanel<'a> {
     diffs: &'a Diffs,
@@ -16,55 +16,63 @@ impl<'a> PathsPanel<'a> {
             selected_key,
         }
     }
+
+    fn show_keys<P>(&mut self, ui: &mut Ui, action: &mut Option<Action>, predicate: P)
+    where
+        P: FnMut(&&DiffKey) -> bool,
+    {
+        let keys = self.diffs.keys().filter(predicate);
+        for key in keys {
+            let checked = self.selected_key.as_ref() == Some(key);
+            let response = ui.selectable_label(checked, &key.path);
+            let key_pressed = ui.input(|i| i.key_pressed(Key::Enter) || i.key_pressed(Key::Space));
+
+            if response.double_clicked() || (key_pressed && response.has_focus()) {
+                *action = Some(Action::Repo(AddOrRestore(key.clone())));
+                *self.selected_key = None;
+            } else if response.clicked() {
+                *self.selected_key = if checked {
+                    None
+                } else {
+                    response.request_focus();
+                    Some(key.clone())
+                }
+            }
+        }
+    }
 }
 
 impl<'a> Show for PathsPanel<'a> {
     fn show(&mut self, ctx: &Context, action: &mut Option<Action>) {
-        let panel_width = ctx.content_rect().width() * 0.5;
+        let max_panel_width = ctx.content_rect().width() * 0.5;
 
         SidePanel::left("paths")
-            .max_width(panel_width)
+            .max_width(max_panel_width)
             .show(ctx, |ui| {
-                let mut show_keys = |ui: &mut Ui, id, max_height, filter: fn(&&DiffKey) -> bool| {
-                    ScrollArea::both()
-                        .id_salt(id)
-                        .max_height(max_height)
-                        .show(ui, |ui| {
-                            ui.take_available_space();
+                CollapsingHeader::new("unstaged")
+                    .default_open(true)
+                    .show(ui, |ui| {
+                        let max_scroll_height = ui.available_height() * 0.5;
 
-                            let keys = self.diffs.keys().filter(filter);
-                            for key in keys {
-                                let checked = self.selected_key.as_ref() == Some(key);
-                                let response = ui.selectable_label(checked, &key.path);
-                                let key_pressed = ui.input(|i| {
-                                    i.key_pressed(Key::Enter) || i.key_pressed(Key::Space)
-                                });
+                        ScrollArea::both()
+                            .id_salt("unstaged_scroll")
+                            .max_height(max_scroll_height)
+                            .show(ui, |ui| {
+                                ui.take_available_space();
+                                self.show_keys(ui, action, DiffKey::is_not_staged);
+                            });
+                    });
 
-                                if response.double_clicked()
-                                    || (key_pressed && response.has_focus())
-                                {
-                                    *action = Some(Action::Repo(AddOrRestore(key.clone())));
-                                    *self.selected_key = None;
-                                } else if response.clicked() {
-                                    *self.selected_key = if checked {
-                                        None
-                                    } else {
-                                        response.request_focus();
-                                        Some(key.clone())
-                                    }
-                                }
-                            }
-                        });
-                };
-
-                show_keys(
-                    ui,
-                    "unstaged",
-                    ui.available_height() / 2.0,
-                    DiffKey::is_not_staged,
-                );
                 ui.separator();
-                show_keys(ui, "staged", ui.available_height(), DiffKey::is_staged);
+
+                CollapsingHeader::new("staged")
+                    .default_open(true)
+                    .show(ui, |ui| {
+                        ScrollArea::both().id_salt("staged_scroll").show(ui, |ui| {
+                            ui.take_available_space();
+                            self.show_keys(ui, action, DiffKey::is_staged);
+                        });
+                    });
             });
     }
 }
