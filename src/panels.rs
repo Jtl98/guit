5ec7035d5a8b16d5d9d7ed1694c0afc075bf7fcf
josh <joshualewisvic@gmail.com>
2026-03-22 19:33:17 +1100

    refactor top panel and use guards

--- src/panels.rs
+++ src/panels.rs
@@ -7,8 +7,18 @@ pub mod top;
 pub mod welcome;
 
 use crate::common::Action;
-use eframe::egui::Context;
+use eframe::egui::{Button, Context, Response, Ui};
 
 pub trait Show {
     fn show(&mut self, ctx: &Context, action: &mut Option<Action>);
+}
+
+trait AddWidget {
+    fn executing_button(&mut self, is_executing: bool, text: &str) -> Response;
+}
+
+impl AddWidget for Ui {
+    fn executing_button(&mut self, is_executing: bool, text: &str) -> Response {
+        self.add_enabled(!is_executing, Button::new(text))
+    }
 }
--- src/panels/top.rs
+++ src/panels/top.rs
@@ -4,9 +4,9 @@ use crate::{
         FileAction::Close,
         RepoAction::{Create, Fetch, Pull, Push, Refresh, Stash, Switch},
     },
-    panels::Show,
+    panels::{AddWidget, Show},
 };
-use eframe::egui::{Align, Button, ComboBox, Context, Key, Layout, TextEdit, TopBottomPanel};
+use eframe::egui::{Align, Button, ComboBox, Context, Key, Layout, TextEdit, TopBottomPanel, Ui};
 
 pub struct TopPanel<'a> {
     is_executing: &'a bool,
@@ -29,44 +29,71 @@ impl<'a> TopPanel<'a> {
             branch_search,
         }
     }
+
+    fn show_branches(&mut self, ui: &mut Ui, action: &mut Option<Action>) {
+        let branch_box = ComboBox::from_id_salt("branches")
+            .selected_text(&self.branches.current)
+            .show_ui(ui, |ui| {
+                ui.take_available_height();
+                ui.text_edit_singleline(self.branch_search).request_focus();
+
+                for branch in &self.branches.other {
+                    let searched_branch = self.branch_search.to_lowercase();
+                    if !branch.name.to_lowercase().contains(&searched_branch) {
+                        continue;
+                    }
+
+                    if ui.selectable_label(false, branch.to_string()).clicked() {
+                        *action = Some(Action::Repo(Switch(branch.clone())));
+                    }
+                }
+            });
+
+        if branch_box.response.clicked() {
+            self.branch_search.clear();
+        }
+
+        let branch_name = self.branch_name.get_or_insert_default();
+        let branch_name_provided = !branch_name.trim().is_empty();
+        let button = ui.add_enabled(!self.is_executing && branch_name_provided, Button::new("+"));
+        let text = ui.add_enabled(!self.is_executing, TextEdit::singleline(branch_name));
+
+        if !branch_name_provided {
+            return;
+        }
+
+        let key_pressed = text.lost_focus() && ui.input(|i| i.key_pressed(Key::Enter));
+        if !button.clicked() && !key_pressed {
+            return;
+        }
+
+        if let Some(branch_name) = self.branch_name.take() {
+            *action = Some(Action::Repo(Create(branch_name)));
+        }
+    }
 }
 
 impl<'a> Show for TopPanel<'a> {
     fn show(&mut self, ctx: &Context, action: &mut Option<Action>) {
         TopBottomPanel::top("top").show(ctx, |ui| {
             ui.horizontal(|ui| {
-                if ui
-                    .add_enabled(!self.is_executing, Button::new("refresh"))
-                    .clicked()
-                {
+                if ui.executing_button(*self.is_executing, "refresh").clicked() {
                     *action = Some(Action::Repo(Refresh));
                 }
 
-                if ui
-                    .add_enabled(!self.is_executing, Button::new("fetch"))
-                    .clicked()
-                {
+                if ui.executing_button(*self.is_executing, "fetch").clicked() {
                     *action = Some(Action::Repo(Fetch));
                 }
 
-                if ui
-                    .add_enabled(!self.is_executing, Button::new("pull"))
-                    .clicked()
-                {
+                if ui.executing_button(*self.is_executing, "pull").clicked() {
                     *action = Some(Action::Repo(Pull));
                 }
 
-                if ui
-                    .add_enabled(!self.is_executing, Button::new("push"))
-                    .clicked()
-                {
+                if ui.executing_button(*self.is_executing, "push").clicked() {
                     *action = Some(Action::Repo(Push));
                 }
 
-                if ui
-                    .add_enabled(!self.is_executing, Button::new("stash"))
-                    .clicked()
-                {
+                if ui.executing_button(*self.is_executing, "stash").clicked() {
                     *action = Some(Action::Repo(Stash));
                 }
 
@@ -75,42 +102,7 @@ impl<'a> Show for TopPanel<'a> {
                         *action = Some(Action::File(Close));
                     }
 
-                    let branch_box = ComboBox::from_id_salt("branches")
-                        .selected_text(&self.branches.current)
-                        .show_ui(ui, |ui| {
-                            ui.take_available_height();
-                            ui.text_edit_singleline(self.branch_search).request_focus();
-
-                            for branch in &self.branches.other {
-                                let searched_branch = self.branch_search.to_lowercase();
-                                if !branch.name.to_lowercase().contains(&searched_branch) {
-                                    continue;
-                                }
-
-                                if ui.selectable_label(false, branch.to_string()).clicked() {
-                                    *action = Some(Action::Repo(Switch(branch.clone())));
-                                }
-                            }
-                        });
-
-                    if branch_box.response.clicked() {
-                        self.branch_search.clear();
-                    }
-
-                    let branch_name = self.branch_name.get_or_insert_default();
-                    let branch_name_provided = !branch_name.trim().is_empty();
-                    let button = ui
-                        .add_enabled(!self.is_executing && branch_name_provided, Button::new("+"));
-                    let text =
-                        ui.add_enabled(!self.is_executing, TextEdit::singleline(branch_name));
-                    let key_pressed = text.lost_focus() && ui.input(|i| i.key_pressed(Key::Enter));
-
-                    if branch_name_provided
-                        && (button.clicked() || key_pressed)
-                        && let Some(branch_name) = self.branch_name.take()
-                    {
-                        *action = Some(Action::Repo(Create(branch_name)));
-                    }
+                    self.show_branches(ui, action);
                 });
             });
         });
