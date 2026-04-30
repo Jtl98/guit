c624a7328bb2613f10f6efe6e513b8fd680fef65
Jtl98 <54021623+Jtl98@users.noreply.github.com>
2026-04-30 17:59:56 +1000

    fix binary file diff (#80)

--- src/panels/diff.rs
+++ src/panels/diff.rs
@@ -1,8 +1,8 @@
 use crate::{
-    common::{Action, Diff},
+    common::{Action, Diff, StringDiff},
     panels::Show,
 };
-use eframe::egui::{CentralPanel, Color32, Context, Label, RichText, ScrollArea, TextWrapMode};
+use eframe::egui::{CentralPanel, Color32, Context, Label, RichText, ScrollArea, TextWrapMode, Ui};
 
 pub struct DiffPanel<'a> {
     diff: &'a Diff,
@@ -12,36 +12,49 @@ impl<'a> DiffPanel<'a> {
     pub fn new(diff: &'a Diff) -> Self {
         Self { diff }
     }
+
+    fn show_binary(&self, ui: &mut Ui) {
+        ui.horizontal_wrapped(|ui| {
+            ui.label("binary file");
+        });
+
+        ui.separator();
+    }
+
+    fn show_string(&self, ui: &mut Ui, diff: &StringDiff) {
+        ui.horizontal_wrapped(|ui| {
+            ui.colored_label(Color32::GREEN, &diff.numstat.additions);
+            ui.colored_label(Color32::RED, &diff.numstat.deletions);
+        });
+
+        ui.separator();
+
+        ScrollArea::both().show(ui, |ui| {
+            ui.take_available_space();
+
+            for line in diff.content.lines() {
+                let colour = if line.starts_with('+') {
+                    Color32::GREEN
+                } else if line.starts_with('-') {
+                    Color32::RED
+                } else {
+                    ui.visuals().text_color()
+                };
+
+                ui.add(
+                    Label::new(RichText::new(line).monospace().color(colour))
+                        .wrap_mode(TextWrapMode::Extend),
+                );
+            }
+        });
+    }
 }
 
 impl<'a> Show for DiffPanel<'a> {
     fn show(&mut self, ctx: &Context, _action: &mut Option<Action>) {
-        CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal_wrapped(|ui| {
-                ui.colored_label(Color32::GREEN, &self.diff.numstat.additions);
-                ui.colored_label(Color32::RED, &self.diff.numstat.deletions);
-            });
-
-            ui.separator();
-
-            ScrollArea::both().show(ui, |ui| {
-                ui.take_available_space();
-
-                for line in self.diff.content.lines() {
-                    let colour = if line.starts_with('+') {
-                        Color32::GREEN
-                    } else if line.starts_with('-') {
-                        Color32::RED
-                    } else {
-                        ui.visuals().text_color()
-                    };
-
-                    ui.add(
-                        Label::new(RichText::new(line).monospace().color(colour))
-                            .wrap_mode(TextWrapMode::Extend),
-                    );
-                }
-            });
+        CentralPanel::default().show(ctx, |ui| match self.diff {
+            Diff::Binary => self.show_binary(ui),
+            Diff::String(diff) => self.show_string(ui, diff),
         });
     }
 }
